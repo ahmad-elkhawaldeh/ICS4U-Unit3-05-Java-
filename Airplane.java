247fdb2db14d83c0d3d2a51cdd55420df438244a
ahmad-elkhawaldeh <60712751+ahmad-elkhawaldeh@users.noreply.github.com>
2022-01-02 00:09:36 -0500

    Create Airplane.java

---
+++ Airplane.java
@@ -0,0 +1,35 @@
+/*
+* This program is a Airplane class.
+*
+* @author  Ahmad El-khawaldeh
+* @version 1.0
+* @since   2021-12-07
+*/
+
+/**
+ * Airplane.
+ */
+public class Airplane {
+    /**
+    * Speed variable.
+    */
+    private int speed = 0;
+
+    /**
+    * Setter.
+    *
+    * @param newSpeed discription.
+    */
+    public void setSpeed(final int newSpeed) {
+        this.speed = newSpeed;
+    }
+
+    /**
+    * Gerter.
+    *
+    * @return returnValue good.
+    */
+    public int getSpeed() {
+        return this.speed;
+    }
+}
